15cc77fb6412a31b315ee6269a1b5dae7900b16b
Sukanyapenaganti <sukanyapenaganti@gmail.com>
2025-12-27 11:25:05 +0530

    first commit

--- train_model.py
+++ train_model.py
@@ -1,17 +1,16 @@
-import pickle
 from sklearn.datasets import load_wine
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+import pickle
 
 # Load dataset
 X, y = load_wine(return_X_y=True)
 
 # Train model
-model = DecisionTreeClassifier()
+model = RandomForestClassifier(random_state=42)
 model.fit(X, y)
 
-# Save model correctly
+# Save model properly
 with open("wine_model.pkl", "wb") as f:
     pickle.dump(model, f)
 
-print("✅ Model saved successfully")
+print("✅ wine_model.pkl saved successfully")
